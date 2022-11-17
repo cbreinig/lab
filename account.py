43512fa75e1cfb3011d80b69081dcdc583b43401
cbreinig <79679379+cbreinig@users.noreply.github.com>
2022-11-17 15:35:58 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,1 +1,12 @@
+def __init__ (self, name):
+  self.account_name = name
+  self.account_balance = balance
 
+def deposit(self, amount):
+  self.account_amount = amount
+  
+def withdraw(self, amount)
+  self.account_amount = amount
+  
+def get_balance(self)
+  return self.account_balance
